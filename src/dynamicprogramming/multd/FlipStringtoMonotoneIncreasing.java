0747c807ec7becc32e4039d433a779d58c7836de
user54778 <ahronrocks51@gmail.com>
2023-01-16 20:36:27 -0500

    lc daily; 2d dp

---
+++ src/dynamicprogramming/multd/FlipStringtoMonotoneIncreasing.java
@@ -0,0 +1,23 @@
+package dynamicprogramming.multd;
+
+public class FlipStringtoMonotoneIncreasing {
+    public int minFlipsMonoIncr(String s) {
+        // two states -> remaining input string, current bit value
+        int[][] dp = new int[s.length() + 1][2];
+
+        // base case -> min number of flips is 0 characters; empty string
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = i;
+            dp[i][1] = i;
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            // recurrence -> 1 is in wrong place
+            dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][0]) + (s.charAt(i - 1) == '1' ? 1 : 0);
+            // recurrence -> 0 is in the wrong place
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]) + (s.charAt(i - 1) == '0' ? 1 : 0);
+        }
+
+        return Math.min(dp[s.length()][0], dp[s.length()][1]);
+    }
+}
